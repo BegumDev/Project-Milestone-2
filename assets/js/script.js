e46e6af98bb75ca4ebe4e5e2da52e6dbbae4cb7f
BegumDev <ruk90@outlook.com>
2022-04-04 18:34:57 +0000

    Add word array, start game function and timer function

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,20 +2,65 @@
 window.addEventListener('load', startGame)
 
 // Add global variables
-const timer = document.querySelector('.timer')
-const wordDisplay = document.querySelector('.word-display')
-const wordInput = document.querySelector('.word-input')
-const gameMessage = document.querySelector('.message')
-const scoreDisplay = document.querySelector('score')
-
-let timer = 10;
+let timer = 50;
 let scoreDisplay = 0
 let isPlaying;
 
+// DOM constants
+const timeDisplay = document.querySelector('.timer')
+const wordDisplay = document.querySelector('.word-display')
+const wordInput = document.querySelector('.word-input')
+const gameMessage = document.querySelector('.message')
+const showScore = document.querySelector('score')
 
+const wordArray = [
+    'Imagine',
+    'Build',
+    'Develop',
+    'Produce',
+    'Explain',
+    'Construct',
+    'Create',
+    'Innovate',
+    'Succeed',
+    'Prepare',
+    'Adjust',
+    'Understand',
+    'Play',
+    'Excel',
+    'Enjoy',
+    'Relax',
+    'Assemble',
+    'Code',
+    'Logic',
+    'Technical',
+    'Spectacular',
+    'Arrange',
+    'Discover',
+    'Reality',
+    'Explore',
+    'Value',
+    'Platform',
+    'Educate',
+    'Podium',
+    'Win',
+]
 
-
-
+// Start the game
 function startGame(){
-    console.log('Ready')
+// Generate a random word
+    const randomWord = Math.floor(Math.random() * wordArray.length);
+// Display the random word    
+    wordDisplay.innerHTML = wordArray[randomWord]
+// Set timer countdown
+    setInterval(countdown, 1000)
+}
+
+function countdown() {
+    if(timer > 0) {
+        timer--;
+    } else if(timer === 0) {
+        isPlaying = false;
+    }
+    timeDisplay.innerHTML = timer;
 }
