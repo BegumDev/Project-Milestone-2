640e633bc3bf3f115eb4195c6b269cc21a1da6a5
BegumDev <ruk90@outlook.com>
2022-04-09 10:55:39 +0000

    Integrate email.js into contact form

--- assets/js/email.js
+++ assets/js/email.js
@@ -1,1 +1,15 @@
-console.log('hello')
+function sendMail(params) {
+    let parameters = {
+        from_name:document.getElementById('name').value,
+        from_email:document.getElementById('email').value,
+        project_request:document.getElementById('message').value,
+    };
+
+    emailjs.send('gmail', 'Test email',parameters)
+    .then(function(response){
+        console.log('Success', response)
+    }),
+    function(error) {
+        console.log('Error', response)
+    }
+}
