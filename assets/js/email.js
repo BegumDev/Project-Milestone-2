077079edd8ed012e7ba7c07a7de1056e2d721ebd
BegumDev <ruk90@outlook.com>
2022-04-12 21:37:40 +0000

    Fix bug in contact form which kept allowing emails to be sent without having filled out all fields of the form

--- assets/js/email.js
+++ assets/js/email.js
@@ -1,15 +1,33 @@
-function sendMail(params) {
-    let parameters = {
-        from_name:document.getElementById('name').value,
-        from_email:document.getElementById('email').value,
-        project_request:document.getElementById('message-box').value,
-    };
+// function sendMail(params) {
+//     let parameters = {
+//         from_name:document.getElementById('name').value,
+//         from_email:document.getElementById('email').value,
+//         project_request:document.getElementById('message').value,
+//     };
 
-    emailjs.send('gmail', 'Test email',parameters)
-    .then(function(response){
-        console.log('Success', response)
-    }),
-    function(error) {
-        console.log('Error', response)
-    }
+//     emailjs.send('gmail', 'Test email',parameters)
+//     .then(function(response){
+//         console.log('Success', response);
+//     }),
+//     function(error) {
+//         console.log('Error', response);
+//     }
+//     return false;
+// }
+
+function sendMail(contactForm) {
+    emailjs.send('gmail', 'Test email', {
+        'from_name':contactForm.name.value,
+        'from_email':contactForm.emailaddress.value,
+        'project_request':contactForm.message.value,
+    })
+    .then(
+        function(response) {
+            console.log('Success', response);
+        },
+        function (error){
+            console.log('Failed', error);
+        }
+    );
+    return false;
 }
