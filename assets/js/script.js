b0fda09b8afbddc8c17511d0cbe45101a1d37feb
BegumDev <ruk90@outlook.com>
2022-04-21 18:27:13 +0000

    Run code through Jshint and add missing semi-colons

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,24 +1,24 @@
 // On load, initialise the game
-document.addEventListener('DOMContentLoaded', intro)
+document.addEventListener('DOMContentLoaded', intro);
 
 //===================================================================================
 
 // DOM constants
-const timeDisplay = document.querySelector('.timer')
-const wordDisplay = document.querySelector('.word-display')
-const wordInput = document.querySelector('.word-input')
-const gameMessage = document.querySelector('.message')
-const scoreDisplay = document.querySelector('#score')
-const closeInstruction = document.querySelector('.close')
-const showInstruction = document.querySelector('.help-btn')
-const restartBtn = document.querySelector('.restart-btn')
-const introDisplay = document.querySelector('.introBtn')
-const modalDisplay = document.querySelector('.modal-container')
+const timeDisplay = document.querySelector('.timer');
+const wordDisplay = document.querySelector('.word-display');
+const wordInput = document.querySelector('.word-input');
+const gameMessage = document.querySelector('.message');
+const scoreDisplay = document.querySelector('#score');
+const closeInstruction = document.querySelector('.close');
+const showInstruction = document.querySelector('.help-btn');
+const restartBtn = document.querySelector('.restart-btn');
+const introDisplay = document.querySelector('.introBtn');
+const modalDisplay = document.querySelector('.modal-container');
 
 // Event listeners
-closeInstruction.addEventListener('click', closeInstructions)
-showInstruction.addEventListener('click', showInstructions)
-restartBtn.addEventListener('click', restart)
+closeInstruction.addEventListener('click', closeInstructions);
+showInstruction.addEventListener('click', showInstructions);
+restartBtn.addEventListener('click', restart);
 
 // Add global variables
 let timer = 6;
@@ -57,32 +57,32 @@ const wordArray = [
     'Educate',
     'Podium',
     'Win',
-]
+];
 
 //===================================================================================
 // Trigger the modal with user control of starting the game
 function intro() {
     // Listen out for the click, once clicked hide the modal and start the game
     introDisplay.addEventListener('click', () => {
-        modalDisplay.classList.add('hide')
+        modalDisplay.classList.add('hide');
         startGame();
-    })
+    });
 }
 //Display the words
 function showWords() {
     // Generate a random word
     const randomWord = Math.floor(Math.random() * wordArray.length);
     // Display the random word    
-    wordDisplay.innerHTML = wordArray[randomWord]
+    wordDisplay.innerHTML = wordArray[randomWord];
 }
 // Start the game
 function startGame() {
     // Display the words
-    showWords()
+    showWords();
     // Focus on the input box
-    wordInput.focus()
+    wordInput.focus();
     // Start the timer
-    countdown()
+    countdown();
     // Check the users input against the word displayed
     wordInput.addEventListener('input', function () {
 
@@ -90,7 +90,7 @@ function startGame() {
             isPlaying = true;
             timer = 6;
             wordInput.value = '';
-            showWords()
+            showWords();
             score++;
         }
         scoreDisplay.innerHTML = `Score: ${score}`;
@@ -100,30 +100,30 @@ function startGame() {
         } else {
             scoreDisplay.innerHTML = `Score: ${score}`;
         }
-    })
+    });
 }
 // Countdown timer
 function countdown() {
     gameInterval = setInterval(() => {
         if (timer > 0) {
             timer--;
-            console.log('on') // Keep this here to check timer is still working
+            console.log('on'); // Keep this here to check timer is still working
         } else if (timer === 0) {
-            clearInterval(gameInterval)
+            clearInterval(gameInterval);
             gameMessage.innerHTML = 'Time is up!';
             wordInput.classList.add('hide');
             scoreDisplay.innerHTML = `You scored: ${score}!`;
             score = -1;
             isPlaying = false;
-            console.log('off') // Keep this here to check timer is still working
+            console.log('off'); // Keep this here to check timer is still working
         }
         timeDisplay.innerHTML = timer;
-    }, 1000)
+    }, 1000);
 }
 // Check if its a match
 function checkMatch() {
     if (wordInput.value === wordDisplay.innerHTML) {
-        gameMessage.innerHTML = 'Well done!'
+        gameMessage.innerHTML = 'Well done!';
         return true;
     } else {
         gameMessage.innerHTML = 'Typing...';
@@ -135,13 +135,13 @@ function checkMatch() {
 function showInstructions() {
     let help = document.querySelector('.instructions');
     let helpBtn = document.querySelector('.help-btn');
-    const helpDisplay = window.getComputedStyle(help).getPropertyValue('display')
+    const helpDisplay = window.getComputedStyle(help).getPropertyValue('display');
 
     if (helpDisplay === 'none') {
         help.style.display = 'block';
         helpBtn.style.display = 'none';
     } else {
-        help.style.display = 'none'
+        help.style.display = 'none';
         helpBtn.style.display = 'block';
     }
 }
@@ -159,11 +159,11 @@ function closeInstructions() {
 function restart() {
     wordInput.classList.remove('hide');
     score = -1;
-    timer = 6
-    clearInterval(gameInterval)
-    countdown()
+    timer = 6;
+    clearInterval(gameInterval);
+    countdown();
     showWords();
-    wordInput.focus()
+    wordInput.focus();
     gameMessage.innerHTML = "";
     wordInput.value = '';
     scoreDisplay.innerHTML = `Score: 0`;
