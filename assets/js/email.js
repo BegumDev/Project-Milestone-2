24564dcd2ea29c34223e83c51f0f7d29f2e20b73
BegumDev <ruk90@outlook.com>
2022-05-03 10:39:35 +0000

    Take out console log and an error msg instead

--- assets/js/email.js
+++ assets/js/email.js
@@ -1,10 +1,16 @@
 const formClear = document.querySelector('.form')
 
 let submitMsg = document.querySelector('.contact-msg')
+let errorMsg = document.querySelector('.error-msg')
 
 function submitResponse() {
     let submitMsg = document.querySelector('.contact-msg');
     submitMsg.innerHTML = `Thank you for submitting an email, we will be in touch with you shortly.`
+}
+
+function errorResponse() {
+    let errorMsg = document.querySelector('.error-msg');
+    errorMsg.innerHTML = "Sorry that didn't quite work. Please try again";
 }
 
 function sendMail(contactForm) {
@@ -20,7 +26,7 @@ function sendMail(contactForm) {
             submitResponse();
         },
         function (error){
-            // error message display error response error msg
+            errorResponse();
         }
     );
     return false;
